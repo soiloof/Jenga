149ab9904aec116aed9b447e3dbb7f7f6ebd27a7
soiloof <tsucchy0718@gmail.com>
2020-01-22 13:25:30 +0900

    jenga done

---
+++ Assets/Script/Jenga.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Jenga : MonoBehaviour
+{
+    public void OnMouseDrag(){
+        Vector3 TapPos = Input.mousePosition;
+        TapPos.z = 10f;
+        transform.position = Camera.main.ScreenToWorldPoint(TapPos);
+    }
+}
